a2f2bebe998d7f9ca1108b0d8ae34360cfb9c10e
Cédric HT <nope-mcnopeface@asdf.org>
2020-07-22 11:26:09 +0200

    More logs for discord auth

--- sni/discord/commands.py
+++ sni/discord/commands.py
@@ -11,7 +11,7 @@ import sni.conf as conf
 from sni.user.models import User
 
 from .discord import complete_authentication_challenge
-from .bot import bot
+from .bot import bot, log
 from .jobs import update_discord_user
 
 
@@ -27,9 +27,14 @@ async def auth(ctx: Context, code: str):
         complete_authentication_challenge(member, code)
         usr: User = User.objects(discord_user_id=member.id).get()
         await update_discord_user(usr)
+        logging.info('Successfully authenticated user %s (%d)',
+                     ctx.author.name, ctx.author.id)
         await ctx.channel.send(
             f'Hi <@{ctx.author.id}> :wave:, you are now authenticated. '
             'Welcome aboard!')
+        await log(
+            f':white_check_mark: Successfully authenticated <@{ctx.author.id}>'
+        )
     except Exception as error:
         logging.error('Could not authenticate Discord user %s (%d): %s',
                       ctx.author.name, ctx.author.id, str(error))
@@ -37,4 +42,6 @@ async def auth(ctx: Context, code: str):
             f':x: Sorry <@{ctx.author.id}>, I could not authenticate you. '
             'Please try again with a different code. If the problem persists, '
             'contact your Discord administrator.')
+        await log(
+            f':x: Failed to authenticate <@{ctx.author.id}>: {str(error)}')
     await ctx.message.delete()
